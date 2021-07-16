12b8f08a77d03b60d6ca6bf30f850f00592600ce
Masashi <t15519ms@gmail.com>
2021-07-16 11:31:36 +0900

    [WIP]APIの申請中。返答が到着し次第対応予定

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -11,6 +11,27 @@ class LinebotController < ApplicationController
     }
   end
 
+  def callback
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+
+    unless client.validate_signature(body, signature)
+      head :bad_request
+    end
+
+    events = client.parse_events_from(body)
+
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+
+        end
+      end
+    end
+  end
+
   
   # おうむ返しBotを作成した時のcallbackメソッド
   # def callback
